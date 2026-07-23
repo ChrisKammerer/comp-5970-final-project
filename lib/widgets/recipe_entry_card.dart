3386f3a1570494db71565bab0e1c6e9998464ea5
Chris Kammerer <ckamm001@outlook.com>
2026-07-23 14:32:35 -0500

    add text wrapping and header

--- lib/widgets/recipe_entry_card.dart
+++ lib/widgets/recipe_entry_card.dart
@@ -1,3 +1,4 @@
+import "package:auto_size_text/auto_size_text.dart";
 import "package:flutter/material.dart";
 import "../models/recipe_entry.dart";
 
@@ -14,54 +15,58 @@ class RecipeEntryCard extends StatelessWidget {
       clipBehavior: Clip.antiAlias,
       elevation: 4.0,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      child: Container(
-        decoration: BoxDecoration(
-          color: _isDefaultImage ? colorScheme.primaryContainer : null,
-          image: _isDefaultImage
-              ? null
-              : DecorationImage(
-                  image: recipeEntry.image,
-                  fit: BoxFit.cover,
-                  colorFilter: ColorFilter.mode(
-                    Colors.black.withValues(alpha: 0.35),
-                    BlendMode.darken,
-                  ),
-                ),
-        ),
-        child: Stack(
-          fit: StackFit.expand,
+      child: InkWell(
+        onTap: () {
+          // Navigator.push(
+          //     context,
+          //     MaterialPageRoute(
+          //       builder: (context) => RecipeEntryDetail(),
+          //     ),
+          //   );
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            if (_isDefaultImage)
-              Center(
-                child: Icon(
-                  Icons.menu_book_rounded,
-                  size: 64,
-                  color: colorScheme.onPrimaryContainer.withValues(alpha: 0.5),
+            Container(
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 8.0,
+              ),
+              child: AutoSizeText(
+                recipeEntry.name,
+                maxLines: 2,
+                minFontSize: 14,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black87,
                 ),
               ),
-            InkWell(
-              onTap: () {
-                // Navigator.push(
-                //     context,
-                //     MaterialPageRoute(
-                //       builder: (context) => RecipeEntryDetail(),
-                //     ),
-                //   );
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      recipeEntry.name,
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: _isDefaultImage ? colorScheme.primaryContainer : null,
+                  image: _isDefaultImage
+                      ? null
+                      : DecorationImage(
+                          image: recipeEntry.image,
+                          fit: BoxFit.cover,
+                        ),
                 ),
+                child: _isDefaultImage
+                    ? Center(
+                        child: Icon(
+                          Icons.menu_book_rounded,
+                          size: 64,
+                          color: colorScheme.onPrimaryContainer.withValues(
+                            alpha: 0.5,
+                          ),
+                        ),
+                      )
+                    : null,
               ),
             ),
           ],
